a3553fefdd09f6518a713d6e0779ed4348cb656f
antodup <anthony_dupre@live.fr>
2017-01-07 09:57:55 +0100

    version 0.9

--- nike-javascript.js
+++ nike-javascript.js
@@ -1,3 +1,13 @@
+/*============================================================
+#title            : Exercise 7: INTGRATION NIKE
+#description      : integration with HTML/CSS/JAVASCRIPT
+#author           : DUPRE Anthony
+#date             : 12/12/2016
+#version          : Constantly on progress
+#usage            : JAVASCRIPT
+#notes            : 
+=============================================================*/
+
 $(document).ready(function(){
     
     var heart = document.getElementById('heart'), 
@@ -9,8 +19,20 @@ $(document).ready(function(){
         add1 = document.getElementById('add1'),
         add2 = document.getElementById('add2'),
         number = document.getElementById('number-cart'),
-        goal = 0;
+        goal = 0, 
+        
+        body = document.getElementsByTagName("body")[0],
+        
+        search = document.getElementById('search');
 
+    function inputsearch (e){
+        if (e.keyCode == 13){
+            search.value = "";
+       }
+    }
+    
+    addEventListener('keydown', inputsearch);
+    
     function change_heart (){
         if(heart_change == 1){
             heart_change = 0 ;
@@ -41,14 +63,46 @@ $(document).ready(function(){
     
     function addcart () {
         if (goal == 0){
-            ++ goal;
+            ++goal;
             $('#number-cart').html( "(" + goal + ")" ); 
-            console.log(goal); 
+            
+            $('#add1').html( "REMOVE TO CART"); 
+            $('#add2').html( "REMOVE TO CART"); 
+        } else {
+            --goal; 
+            $('#number-cart').html( "(" + goal + ")" );
+            $('#add1').html( "ADD TO CART"); 
+            $('#add2').html( "ADD TO CART");
         }
     }
     
     add1.addEventListener("click", addcart)
     add2.addEventListener("click", addcart)
+
+    window.onscroll = function(){
+       if (document.body.scrollTop >= 0 && document.body.scrollTop < 730){
+            $('#article2').css({
+                position : "absolute", 
+                top : "118px", 
+                right : "30px"
+            });
+       } else if (document.body.scrollTop >= 730 && document.body.scrollTop <= 1400){
+           $('#article2').css({
+                position : "fixed", 
+                top : "118px", 
+                right : "80px"
+            }); 
+       } else if (document.body.scrollTop > 1400){
+           $('#article2').css({
+                position : "absolute", 
+                top : "780px", 
+                right : "30px"
+            });
+       }
+    }
     
-        
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 30, 
+    });
 });
