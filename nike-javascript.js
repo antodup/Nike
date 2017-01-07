36a5468b8692b05b6ca3082d136cb79efabe0ca1
antodup <anthony_dupre@live.fr>
2017-01-07 19:36:32 +0100

    version 1.1

--- nike-javascript.js
+++ nike-javascript.js
@@ -1,5 +1,5 @@
 /*============================================================
-#title            : Exercise 7: INTGRATION NIKE
+#title            : Exercise 7: INTEGRATION NIKE
 #description      : integration with HTML/CSS/JAVASCRIPT
 #author           : DUPRE Anthony
 #date             : 12/12/2016
@@ -10,29 +10,20 @@
 
 $(document).ready(function(){
     
-    var heart = document.getElementById('heart'), 
-        heart_change = 1, 
-        
-        greychoose = document.getElementById("grey-choose"), 
-        pinkchoose = document.getElementById("pink-choose"), 
-        
-        add1 = document.getElementById('add1'),
-        add2 = document.getElementById('add2'),
-        number = document.getElementById('number-cart'),
+    var heart_change = 1, 
         goal = 0, 
-        
         body = document.getElementsByTagName("body")[0],
-        
-        search = document.getElementById('search');
-
+        cookie_cart = getCookie("NIKEAIRMAX");
+    
+    //SEARCH
     function inputsearch (e){
         if (e.keyCode == 13){
-            search.value = "";
-       }
+            search.value = "";}
     }
     
-    addEventListener('keydown', inputsearch);
+    $("#search").keypress(inputsearch);
     
+    //HEART
     function change_heart (){
         if(heart_change == 1){
             heart_change = 0 ;
@@ -43,15 +34,16 @@ $(document).ready(function(){
         }
     }
    
-    heart.addEventListener('click', change_heart);
+    $("#heart").click(change_heart);
     
+    //CHANGE CHOOSE
     function change_choose1 (){
         $("#choosenike").attr('src', 'PICTURES/top-background-2.png');
         
         $("#choose-nike2").attr('src', 'PICTURES/top-background-2.png');
     }
     
-    greychoose.addEventListener("click", change_choose1)
+    $("#grey-choose").click(change_choose1);
     
     function change_choose2 (){
         $("#choosenike").attr('src', 'PICTURES/top-background-1.png');
@@ -59,7 +51,14 @@ $(document).ready(function(){
         $("#choose-nike2").attr('src', 'PICTURES/top-background-1.png');
     }
     
-    pinkchoose.addEventListener("click", change_choose2)
+    $("#pink-choose").click(change_choose2);
+    
+    //ADD CART & REMOVE
+    
+    if (cookie_cart != null){
+            addcart();
+            console.log(goal); 
+        }
     
     function addcart () {
         if (goal == 0){
@@ -67,18 +66,25 @@ $(document).ready(function(){
             $('#number-cart').html( "(" + goal + ")" ); 
             
             $('#add1').html( "REMOVE TO CART"); 
-            $('#add2').html( "REMOVE TO CART"); 
+            $('#add2').html( "REMOVE TO CART");
+            
+            setCookie("NIKEAIRMAX",goal,30);
+            
         } else {
             --goal; 
             $('#number-cart').html( "(" + goal + ")" );
+            
             $('#add1').html( "ADD TO CART"); 
             $('#add2').html( "ADD TO CART");
-        }
+            
+            eraseCookie("NIKEAIRMAX");
+            }
     }
     
-    add1.addEventListener("click", addcart)
-    add2.addEventListener("click", addcart)
-
+    $("#add1").click(addcart);
+    $("#add2").click(addcart);
+      
+    //SCROLL
     window.onscroll = function(){
        if (document.body.scrollTop >= 0 && document.body.scrollTop < 730){
             $('#article2').css({
@@ -101,6 +107,7 @@ $(document).ready(function(){
        }
     }
     
+    //SWIPPER
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 4,
         spaceBetween: 30, 
